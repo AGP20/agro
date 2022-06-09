a6d068998986a51d17189a9c7abf1fcb4c171640
Leandro Lima <leandro.desouzalima1@gmail.com>
2022-06-08 21:27:52 -0300

    investor backbone ready

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/users/:id', to: 'pages#profile', as: :user_profile
+
+  get '/accepted/:id', to: 'loan_proposals#accept', as: :accept_loan
+  
   resources :users do
     resources :addresses do
     end
